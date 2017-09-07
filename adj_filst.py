0e44eb906ec053a5508ef787523ef739454500e6
andersonfrailey <anderson.frailey@aei.org>
2017-09-07 14:28:40 -0400

    fix adj file

--- adj_filst.py
+++ adj_filst.py
@@ -1,5 +1,4 @@
 import numpy as np
-import copy
 
 
 def adjfilst(cps):
@@ -19,17 +18,17 @@ def adjfilst(cps):
                             (cps['was'] <= 0.), 1, 0)
     np.random.seed(409)
     cps['z1'] = cps.apply(lambda row: np.random.uniform(0, 1)
-                          if row['case1'] == 1 else 1, axis=1)
-    np.random.seed(5410)
+                          if row['case1'] == 1 else 1
+                          if row['case2'] else 0, axis=1)
+    print cps['z1']
+    # np.random.seed(5410)
     cps['z2'] = cps.apply(lambda row: np.random.uniform(0, 1)
-                          if row['case2'] == 1 else 1, axis=1)
-    selected = cps['z1'] <= 0.84 | cps['z2'] <= 0.54
-    cps['filst'] = np.where(selected, 1, cps['filst'])
+                          if row['case2'] == 1 else 1
+                          if row['case1'] else 0, axis=1)
+    # selected = (cps['z1'] <= 0.84 or cps['z2'] <= 0.54)
+    cps['filst'] = np.where((cps['z1'] <= 0.84) | (cps['z2'] <= 0.54),
+                            1, cps['filst'])
     cps.drop(['case1', 'case2'], axis=1, inplace=True)
     cps['cpsseq'] = cps.index + 1
 
-    # Split the CPS into filers and nonfilers
-    filers = copy.deepcopy(cps[cps['filst'] == 1])
-    nonfilers = copy.deepcopy(cps[cps['filst'] == 0])
-
-    return filers, nonfilers
+    return cps

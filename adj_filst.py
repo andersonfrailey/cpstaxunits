5056b584b5a2d16b90ef7e329f04d2c41b48e9a4
andersonfrailey <anderson.frailey@aei.org>
2017-09-07 10:29:12 -0400

    initial files

---
+++ adj_filst.py
@@ -0,0 +1,35 @@
+import numpy as np
+import copy
+
+
+def adjfilst(cps):
+    """
+    Randomly make nonfilers filers
+    Parameters
+    ----------
+    cps: A CPS based tax unit file
+    Returns
+    -------
+    Separate DF's for filers and nonfilers
+    """
+    # Determine which files to change after the adjustment
+    cps['case1'] = np.where((cps['filst'] == 0) &
+                            (cps['was'] > 0.), 1, 0)
+    cps['case2'] = np.where((cps['filst'] == 0) &
+                            (cps['was'] <= 0.), 1, 0)
+    np.random.seed(409)
+    cps['z1'] = cps.apply(lambda row: np.random.uniform(0, 1)
+                          if row['case1'] == 1 else 1, axis=1)
+    np.random.seed(5410)
+    cps['z2'] = cps.apply(lambda row: np.random.uniform(0, 1)
+                          if row['case2'] == 1 else 1, axis=1)
+    selected = cps['z1'] <= 0.84 | cps['z2'] <= 0.54
+    cps['filst'] = np.where(selected, 1, cps['filst'])
+    cps.drop(['case1', 'case2'], axis=1, inplace=True)
+    cps['cpsseq'] = cps.index + 1
+
+    # Split the CPS into filers and nonfilers
+    filers = copy.deepcopy(cps[cps['filst'] == 1])
+    nonfilers = copy.deepcopy(cps[cps['filst'] == 0])
+
+    return filers, nonfilers
